2767ca251d1f2f492e0a9d6c01a662318b24a184
Brian Hamilton <brian@abc.com>
2019-11-24 14:01:53 -0500

    Forward declare a bus just within the olc6502 header for compilation; a CPU owns a bus reference that it may connect to.

--- onyx-dragoon/olc6502.h
+++ onyx-dragoon/olc6502.h
@@ -1,8 +1,15 @@
 #pragma once
 
+class Bus;
+
 class olc6502
 {
     public:
         olc6502();
         ~olc6502();
+
+        void ConnectBus(Bus *n) {bus = n;}
+
+    private:
+        Bus *bus = nullptr;
 };
