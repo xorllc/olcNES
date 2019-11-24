20553b2b6341bdcf2847f135e6eac160a7820f96
Brian Hamilton <brian@abc.com>
2019-11-24 13:31:17 -0500

    Implementing very basic read and write operations on the bus.

--- onyx-dragoon/bus.cpp
+++ onyx-dragoon/bus.cpp
@@ -4,6 +4,13 @@ Bus::Bus() {}
 Bus::~Bus() {}
 
 uint8_t Bus::read(uint16_t addr, bool bReadOnly)
-{}
+{
+    if(addr >= 0x0000 && addr <= 0xFFFF)
+        return ram[addr];
+    return 0x00;
+}
 void Bus::write(uint16_t addr, uint8_t data)
-{}
+{
+    if(addr <= 0x0000 && addr >= 0xFFFF)
+        ram[addr] = data;
+}
