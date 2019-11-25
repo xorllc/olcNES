8f5622916ac6cc6b054305006647fc5ad23d4767
Brian Hamilton <brian@abc.com>
2019-11-24 20:05:29 -0500

    Add in status register support, define the status enumeration, and provide getters/setters.

--- onyx-dragoon/olc6502.cpp
+++ onyx-dragoon/olc6502.cpp
@@ -14,3 +14,31 @@ void olc6502::write(uint16_t a, uint8_t d)
 {
     bus->write(a, d);
 }
+
+uint8_t olc6502::GetFlag(FLAGS6502 f)
+{
+    return ((status & f) > 1) ? 1 : 0;
+}
+
+/*
+ * Set the specified flag in the status register.
+ */
+void olc6502::SetFlag(FLAGS6502 f, bool v)
+{
+    /*
+     * If we want to set the flag, we need to logical
+     * OR its value with whatever data is in the status
+     * register.
+     */
+    if (v)
+        status |= f;
+
+    /*
+     * If we want to unset the flag, then we want to get
+     * the status register, logical AND the status register
+     * with "all bits except the register set" ("~f"), and
+     * commit that to the status register.
+     */
+    else
+        status &= ~f;
+}
--- onyx-dragoon/olc6502.h
+++ onyx-dragoon/olc6502.h
@@ -8,9 +8,37 @@ class olc6502
         olc6502();
         ~olc6502();
 
+    public:
+
+        uint8_t status = 0x00;  // Status Register. Accessible through a getter/setter and holds values
+                                // from the enumeration above.
+
         void ConnectBus(Bus *n) {bus = n;}
 
+        // The status register stores 8 flags. Ive enumerated these here for ease
+        // of access. You can access the status register directly since its public.
+        // The bits have different interpretations depending upon the context and
+        // instruction being executed.
+        enum FLAGS6502
+        {
+            C = (1 << 0),    // Carry Bit
+            Z = (1 << 1),    // Zero
+            I = (1 << 2),    // Disable Interrupts
+            D = (1 << 3),    // Decimal Mode (unused in this implementation)
+            B = (1 << 4),    // Break
+            U = (1 << 5),    // Unused
+            V = (1 << 6),    // Overflow
+            N = (1 << 7),    // Negative
+        };
+
     private:
+
+        // Convenience functions to access the status register.
+        // Wraps up the bitwise operations to set the values of the status register
+        // depending on the flag we're interested in.
+        uint8_t GetFlag(FLAGS6502 f);
+        void    SetFlag(FLAGS6502 f, bool v);
+
         Bus *bus = nullptr;
         uint8_t read(uint16_t a);
         void write(uint16_t a, uint8_t d);
